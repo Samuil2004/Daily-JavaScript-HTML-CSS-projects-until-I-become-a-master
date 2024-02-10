8028cf3d7403219d45b92d26b3c45866f2e623d8
Samuil2004 <s.kozarov23@acsbg.org>
2024-02-10 10:57:04 +0100

    images are updated and the enter button for search

--- 04-What's-The-Weather/main.js
+++ 04-What's-The-Weather/main.js
@@ -7,6 +7,11 @@ constants.btnSearch.addEventListener("click", function (e) {
   e.preventDefault();
   getJson(constants.input.value);
 });
+constants.input.addEventListener("keypress", function (e) {
+  if (e.key === "Enter") {
+    getJson(constants.input.value);
+  }
+});
 
 constants.btnLocate.addEventListener("click", function (e) {
   e.preventDefault();
