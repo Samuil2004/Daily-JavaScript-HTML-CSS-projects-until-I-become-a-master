dded66b6cb20398f49403e7b8aa6dc0322f5d183
Samuil2004 <s.kozarov23@acsbg.org>
2024-02-11 20:26:33 +0100

    add a new question tab is added

--- 05-FAQ/main.js
+++ 05-FAQ/main.js
@@ -1,20 +1,26 @@
 "use strict";
-
-const btns = document.querySelectorAll(".btn");
+let btns;
+btns = document.querySelectorAll(".btn");
 let symbol;
+const btnAddQuestion = document.querySelector(".btnAddQuestion");
+const addQuestionPanel = document.querySelector(".addNewQuestionPanel");
+const btnSubmitQuestion = document.querySelector(".submit");
+const question = document.querySelector(".questionBox");
+const answer = document.querySelector(".BoxForAnswer");
+const questionsPanel = document.querySelector(".questionsPanel");
+const closeFormButton = document.querySelector(".btnClose");
 
-btns.forEach((btn) => {
-  btn.addEventListener("click", function (e) {
-    e.preventDefault();
+questionsPanel.addEventListener("click", function (e) {
+  const btn = e.target.closest(".btn");
+  if (btn) {
     const foundClass = btn
       .closest(".questionPanel")
       .querySelector('[class*="answerBox"]');
     hideInfo(btn);
     foundClass.classList.toggle("hidden");
     changeSymbol(btn);
-  });
+  }
 });
-
 const hideInfo = function (btn) {
   btns.forEach((btns) => {
     if (btns !== btn) {
@@ -35,3 +41,45 @@ const changeSymbol = function (btn) {
   }
   btn.textContent = symbol;
 };
+
+btnAddQuestion.addEventListener("click", function (e) {
+  e.preventDefault();
+  addQuestionPanel.classList.remove("hidden");
+});
+
+btnSubmitQuestion.addEventListener("click", function (e) {
+  e.preventDefault();
+  if (question.value != "" && answer.value != "") {
+    addNewQuestion(question.value, answer.value);
+    addQuestionPanel.classList.add("hidden");
+    question.value = "";
+    answer.value = "";
+  } else {
+    alert("Please fill in all requred data");
+  }
+});
+
+const addNewQuestion = function (inputedQuestion, inputedAnswer) {
+  const html = `<div class="questionPanel">
+  <div class="panel">
+    <div>
+      <h2 class="question">${inputedQuestion}</h2>
+    </div>
+    <div>
+      <button class="btn">+</button>
+    </div>
+  </div>
+  <div class="answerBox hidden">
+    <p class="answer">
+      ${inputedAnswer}
+    </p>
+  </div>
+</div>`;
+  questionsPanel.insertAdjacentHTML("beforeend", html);
+  btns = document.querySelectorAll(".btn");
+};
+
+closeFormButton.addEventListener("click", function (e) {
+  e.preventDefault();
+  addQuestionPanel.classList.add("hidden");
+});
