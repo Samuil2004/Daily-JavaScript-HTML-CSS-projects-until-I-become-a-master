16e299d3fd9916205d55c6a351aa8276ae0297a0
Samuil2004 <s.kozarov23@acsbg.org>
2024-02-14 17:46:42 +0100

    navigation buttons are added functionality

--- 08-Notes/main.js
+++ 08-Notes/main.js
@@ -2,3 +2,13 @@
 
 const btnNewNote = document.querySelector(".BtnNewLetter");
 const countOfNotes = document.querySelector(".countOfNotes");
+const navBtns = document.querySelectorAll(".btn");
+const frontPage = document.querySelector(".frontPage");
+const pages = document.querySelectorAll(".page");
+
+navBtns.forEach((btn) =>
+  btn.addEventListener("click", function (e) {
+    e.preventDefault();
+    pages.forEach((page) => page.classList.toggle("hidden"));
+  })
+);
