62382d993911ddca62bcaa63ae663a60727dd64e
Samuil2004 <s.kozarov23@acsbg.org>
2024-02-18 16:52:15 +0100

    question type and difficulty are added to the DOM

--- 10-Trivia-Game/main.js
+++ 10-Trivia-Game/main.js
@@ -1,12 +1,26 @@
 "use-strict";
 
+const questionType = document.querySelector(".questionType");
+const difficulty = document.querySelector(".difficulty");
+
 let data = [];
+let questionNum = 0;
 const fetchQuestions = async function () {
-  const fetchedData = await fetch("https://opentdb.com/api.php?amount=10").then(
-    (res) => res.json()
-  );
-  data = fetchedData.results;
-  console.log(data);
+  try {
+    const fetchedData = await fetch(
+      "https://opentdb.com/api.php?amount=10"
+    ).then((res) => res.json());
+    data = await fetchedData.results;
+    console.log(data);
+    printQuestion();
+  } catch (err) {
+    alert(err.message);
+  }
 };
 
 fetchQuestions();
+
+const printQuestion = async function () {
+  questionType.textContent = data[questionNum].category;
+  difficulty.textContent = data[questionNum].difficulty;
+};
