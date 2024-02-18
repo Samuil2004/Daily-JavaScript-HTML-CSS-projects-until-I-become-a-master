7bd1710b51bc5a70203ed1eb8627cf14258718f1
Samuil2004 <s.kozarov23@acsbg.org>
2024-02-18 20:19:36 +0100

    change answer div functiona added

--- 10-Trivia-Game/main.js
+++ 10-Trivia-Game/main.js
@@ -3,6 +3,8 @@
 const questionType = document.querySelector(".questionType");
 const difficulty = document.querySelector(".difficulty");
 const question = document.querySelector(".questionPanel");
+const multipleChoiseQuestion = document.querySelector(".multipleChoise");
+const booleanQuestion = document.querySelector(".boolean");
 
 let data = [];
 let questionNum = 0;
@@ -13,7 +15,8 @@ const fetchQuestions = async function () {
     ).then((res) => res.json());
     data = await fetchedData.results;
     console.log(data);
-    printQuestion();
+    defineQuestion();
+    printQuestionAndAnswers();
   } catch (err) {
     alert(err.message);
   }
@@ -21,10 +24,22 @@ const fetchQuestions = async function () {
 
 fetchQuestions();
 
-const printQuestion = async function () {
+const printQuestionAndAnswers = async function () {
   questionType.textContent = data[questionNum].category;
-  difficulty.textContent = data[questionNum].difficulty;
+  let questionDifficulty = data[questionNum].difficulty;
+  difficulty.textContent =
+    questionDifficulty[0].toUpperCase() + questionDifficulty.slice(1);
   question.textContent = data[questionNum].question;
+};
+
+const defineQuestion = function () {
+  if (data[questionNum].type === "multiple") {
+    multipleChoiseQuestion.classList.remove("hidden");
+    booleanQuestion.classList.add("hidden");
+  } else {
+    multipleChoiseQuestion.classList.add("hidden");
+    booleanQuestion.classList.remove("hidden");
+  }
 };
 
 //change the answer buttons according to to the type of question - boolean or multiple choise
