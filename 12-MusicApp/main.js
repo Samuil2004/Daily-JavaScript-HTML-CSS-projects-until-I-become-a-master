8c97a07e6ec6454c82d1d03d58b90bf7b48f51c2
Samuil2004 <s.kozarov23@acsbg.org>
2024-02-25 18:44:22 +0100

    nav btns added

--- 12-MusicApp/main.js
+++ 12-MusicApp/main.js
@@ -2,19 +2,5 @@
 
 import * as configurator from "./configurator.js";
 import { fetchData } from "./dataManager.js";
-// const tester = async function () {
-//   try {
-//     await fetch(configurator.url, configurator.options)
-//       .then((res) => res.json())
-//       .then((rest) => fillInInfo(rest));
-//   } catch (error) {
-//     console.error(error);
-//   }
-// };
-fetchData();
 
-// const fillInInfo = function (info) {
-//   configurator.img.src = info.data[0].artist.picture_medium;
-//   configurator.songTitle.textContent = info.data[0].title;
-//   configurator.songArtist.textContent = info.data[0].artist.name;
-// };
+fetchData();
