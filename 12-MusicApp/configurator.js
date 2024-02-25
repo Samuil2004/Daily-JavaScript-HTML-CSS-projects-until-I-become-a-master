e1507a166f2c360bf2c60d085a0c66371a60859a
Samuil2004 <s.kozarov23@acsbg.org>
2024-02-25 17:59:03 +0100

    bussines architecture added

---
+++ 12-MusicApp/configurator.js
@@ -0,0 +1,11 @@
+export const img = document.querySelector(".songImage");
+export const songTitle = document.querySelector(".songTitle");
+export const songArtist = document.querySelector(".songArtist");
+export const url = "https://deezerdevs-deezer.p.rapidapi.com/search?q=azis";
+export const options = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "c09033b211msh096e9d7d447afabp1dd192jsnb170c41dabd6",
+    "X-RapidAPI-Host": "deezerdevs-deezer.p.rapidapi.com",
+  },
+};
--- 12-MusicApp/main.js
+++ 12-MusicApp/main.js
@@ -1,20 +1,9 @@
 "use strict";
-const img = document.querySelector(".songImage");
-const songTitle = document.querySelector(".songTitle");
-const songArtist = document.querySelector(".songArtist");
-
-const url = "https://deezerdevs-deezer.p.rapidapi.com/search?q=azis";
-const options = {
-  method: "GET",
-  headers: {
-    "X-RapidAPI-Key": "c09033b211msh096e9d7d447afabp1dd192jsnb170c41dabd6",
-    "X-RapidAPI-Host": "deezerdevs-deezer.p.rapidapi.com",
-  },
-};
+import * as configurator from "./configurator.js";
 
 const tester = async function () {
   try {
-    await fetch(url, options)
+    await fetch(configurator.url, configurator.options)
       .then((res) => res.json())
       .then((rest) => fillInInfo(rest));
   } catch (error) {
@@ -24,7 +13,7 @@ const tester = async function () {
 tester();
 
 const fillInInfo = function (info) {
-  img.src = info.data[0].artist.picture_medium;
-  songTitle.textContent = info.data[0].title;
-  songArtist.textContent = info.data[0].artist.name;
+  configurator.img.src = info.data[0].artist.picture_medium;
+  configurator.songTitle.textContent = info.data[0].title;
+  configurator.songArtist.textContent = info.data[0].artist.name;
 };
