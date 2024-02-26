71a0c7aefd549bcc1e5f14556aeac84d575e8bf3
Samuil2004 <s.kozarov23@acsbg.org>
2024-02-26 12:38:06 +0100

    functinallity added but not working well

--- 12-MusicApp/controlPanel.js
+++ 12-MusicApp/controlPanel.js
@@ -1,8 +1,15 @@
 import * as configurator from "./configurator.js";
-
+let isSongPaused = false;
 export function playPauseBtnFunctionality(e) {
   console.log(e);
-  configurator.pausePlaySongBtn.src === "./images/pauseSongBtnImg.png"
-    ? (configurator.pausePlaySongBtn.src = "./images/playSongBtnImg.png")
-    : (configurator.pausePlaySongBtn.src = "./images/pauseSongBtnImg.png");
+  if (isSongPaused) {
+    configurator.pausePlaySongBtn.src = "./images/playSongBtnImg.png";
+  } else {
+    configurator.pausePlaySongBtn.src = "./images/pauseSongBtnImg.png";
+    isSongPaused = true;
+  }
+
+  // configurator.pausePlaySongBtn.src === "./images/pauseSongBtnImg.png"
+  //   ? (configurator.pausePlaySongBtn.src = "./images/playSongBtnImg.png")
+  //   : (configurator.pausePlaySongBtn.src = "./images/pauseSongBtnImg.png");
 }
