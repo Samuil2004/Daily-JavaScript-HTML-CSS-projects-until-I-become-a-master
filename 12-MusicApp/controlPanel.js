54998b557f13ef214dd45c6d1fad6c6b7ed53434
Samuil2004 <s.kozarov23@acsbg.org>
2024-02-26 12:40:46 +0100

    img source issue

--- 12-MusicApp/controlPanel.js
+++ 12-MusicApp/controlPanel.js
@@ -1,15 +1,15 @@
 import * as configurator from "./configurator.js";
-let isSongPaused = false;
+// let isSongPaused = false;
 export function playPauseBtnFunctionality(e) {
   console.log(e);
-  if (isSongPaused) {
-    configurator.pausePlaySongBtn.src = "./images/playSongBtnImg.png";
-  } else {
-    configurator.pausePlaySongBtn.src = "./images/pauseSongBtnImg.png";
-    isSongPaused = true;
-  }
-
-  // configurator.pausePlaySongBtn.src === "./images/pauseSongBtnImg.png"
-  //   ? (configurator.pausePlaySongBtn.src = "./images/playSongBtnImg.png")
-  //   : (configurator.pausePlaySongBtn.src = "./images/pauseSongBtnImg.png");
+  // if (isSongPaused) {
+  //   configurator.pausePlaySongBtn.src = "./images/playSongBtnImg.png";
+  // } else {
+  //   configurator.pausePlaySongBtn.src = "./images/pauseSongBtnImg.png";
+  //   isSongPaused = true;
+  // }
+  console.log(configurator.pausePlaySongBtn.src);
+  configurator.pausePlaySongBtn.src === "./images/pauseSongBtnImg.png"
+    ? (configurator.pausePlaySongBtn.src = "./images/playSongBtnImg.png")
+    : (configurator.pausePlaySongBtn.src = "./images/pauseSongBtnImg.png");
 }
