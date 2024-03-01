25d7da70be7a6c08c1aa26ae6419f3e44f40127b
Samuil2004 <s.kozarov23@acsbg.org>
2024-03-01 20:36:00 +0100

    refactored code

--- 12-MusicApp/configurator.js
+++ 12-MusicApp/configurator.js
@@ -28,10 +28,3 @@ export const options = {
 };
 
 export const urlForAlbum = "https://deezerdevs-deezer.p.rapidapi.com/album/";
-// const options = {
-// 	method: 'GET',
-// 	headers: {
-// 		'X-RapidAPI-Key': 'c09033b211msh096e9d7d447afabp1dd192jsnb170c41dabd6',
-// 		'X-RapidAPI-Host': 'deezerdevs-deezer.p.rapidapi.com'
-// 	}
-// };
