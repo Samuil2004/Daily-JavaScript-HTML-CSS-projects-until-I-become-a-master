6a8f6164272812da89c15ed0828db222a14340d9
Samuil2004 <s.kozarov23@acsbg.org>
2024-03-06 22:48:19 +0100

    14

--- 12-MusicApp/dataManager.js
+++ 12-MusicApp/dataManager.js
@@ -52,7 +52,6 @@ export function createSong(storedData) {
   currentlyPlayingSong = newSong;
 }
 
-//   configurator.songArtist.textContent = song.artist;
 //   configurator.songTimeLeft.textContent = formatTime(song.duration);
 //   configurator.songDurationBar.max = song.duration;
 // };
