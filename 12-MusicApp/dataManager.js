57fd5144efd9de4161d00d09a7eb7b1bfa3f7631
Samuil2004 <s.kozarov23@acsbg.org>
2024-03-07 09:13:00 +0100

    9

--- 12-MusicApp/dataManager.js
+++ 12-MusicApp/dataManager.js
@@ -11,7 +11,6 @@ let currentSongOpenedFromSearch;
 export function changeSongSource(setToWhat) {
   currentSongOpenedFromSearch = setToWhat;
 }
-//     // .then((rest) => createObject(rest));
 //     // .then((rest) => console.log(rest));
 //   } catch (error) {
 //     console.error(error);
