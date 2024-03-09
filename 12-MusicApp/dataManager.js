e6f87d0e3dfc708e81b3aa3eb768acd18b47d768
Samuil2004 <s.kozarov23@acsbg.org>
2024-03-09 19:15:17 +0100

    15

--- 12-MusicApp/dataManager.js
+++ 12-MusicApp/dataManager.js
@@ -127,7 +127,6 @@ export async function loadAlbum() {
     .then((res) => createAlbum(res));
 }
 
-//     <img src="${song.image}"  class="songImg" />
 //     <h2 class="songName">${song.title}</h2>
 //   </div>
 //   <div class="rightSide">
