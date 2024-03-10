d61e6fce24c00c5cf47088c23cdf466b16e1fcd7
Samuil2004 <s.kozarov23@acsbg.org>
2024-03-10 21:19:01 +0100

    4

--- 12-MusicApp/dataManager.js
+++ 12-MusicApp/dataManager.js
@@ -225,7 +225,6 @@ export function fillInfoForSongSongPage(song) {
   configurator.songDurationBar.max = song.duration;
 }
 
-//     storedData.title,
 //     storedData.artist.name,
 //     storedData.duration,
 //     storedData.album.cover_xl,
