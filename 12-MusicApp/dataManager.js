4d05943e4aa8db49537e01309b43a07052c8e64c
Samuil2004 <s.kozarov23@acsbg.org>
2024-03-11 19:43:27 +0100

    14

--- 12-MusicApp/dataManager.js
+++ 12-MusicApp/dataManager.js
@@ -127,7 +127,6 @@ export async function loadAlbum() {
     .then((res) => createAlbum(res));
 }
 
-//   </div>
 // </dv>`;
 //   configurator.albumSongs.insertAdjacentHTML("beforeend", html);
 // };
